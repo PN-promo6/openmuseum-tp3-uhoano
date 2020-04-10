7d61065c6a0cbccc715aa75234b4c23330810c9e
Jean-Charles <jean-charles.ly@oika.eu>
2020-04-10 17:07:27 +0200

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes , RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SingleMuseumComponent } from './single-museum/single-museum.component';
+import {MuseumListService} from './services/museumList.service'
+import {AddMuseumComponent} from './add-museum/add-museum.component'
+import {HomeComponent} from './home/home.component';
+import {MuseumDetailComponent} from './museum-detail/museum-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+
+const appRoutes:Routes =[
+  { path : 'add', component : AddMuseumComponent},
+  { path : 'singleMuseum',component : SingleMuseumComponent},
+  { path : '' , component : HomeComponent},
+  { path : 'museum/:ref_musee', component : MuseumDetailComponent},
+  { path: 'not-found', component: NotFoundComponent }
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    SingleMuseumComponent,
+    HomeComponent,
+    AddMuseumComponent,
+    MuseumDetailComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [MuseumListService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/single-museum/single-museum.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {MuseumListService} from '../services/museumList.service'
+
+
+@Component({
+  selector: 'app-single-museum',
+  templateUrl: './single-museum.component.html',
+  styleUrls: ['./single-museum.component.css']
+})
+export class SingleMuseumComponent implements OnInit {
+
+  museums : any[];
+  
+  @Input() museumName: string;
+  @Input() adress: string ;
+  @Input() zipCode: string ;
+  @Input() city: string ;
+  @Input() phone: string ;
+  @Input()webSite: string ;
+  @Input() openInfo: string;
+
+  constructor(private museumListService : MuseumListService) {
+
+  }
+
+ngOnInit(){
+this.museums = this.museumListService.museums
+}
+
+
+}
